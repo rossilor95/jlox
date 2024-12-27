c03323a8c6aa68c95aec0f7c600a726cdacf9359
rossilor95 <rossilor95@outlook.it>
2024-12-27 11:56:27 +0100

    feat: add Token interface with implementations

---
+++ src/main/java/com/github/rossilor95/jlox/parse/Token.java
@@ -0,0 +1,56 @@
+package com.github.rossilor95.jlox.parse;
+
+public sealed interface Token {
+    record Eof() implements Token { }
+    record Illegal() implements Token { }
+
+    // Single-character tokens.
+    record LeftParen() implements Token { }
+    record RightParen() implements Token { }
+    record LeftBrace() implements Token { }
+    record RightBrace() implements Token { }
+    record Comma() implements Token { }
+    record Dot() implements Token { }
+    record Semicolon() implements Token { }
+
+    // One or two character tokens.
+    record Bang() implements Token { }
+    record BangEqual() implements Token { }
+    record Equal() implements Token { }
+    record EqualEqual() implements Token { }
+    record Greater() implements Token { }
+    record GreaterEqual() implements Token { }
+    record Less() implements Token { }
+    record LessEqual() implements Token { }
+    record Minus() implements Token { }
+    record MinusEqual() implements Token { }
+    record Plus() implements Token { }
+    record PlusEqual() implements Token { }
+    record Slash() implements Token { }
+    record SlashEqual() implements Token { }
+    record Star() implements Token { }
+    record StarEqual() implements Token { }
+
+    // Identifier and literals.
+    record Identifier(String lexeme) implements Token { }
+    record NumberLiteral(double lexeme) implements Token { }
+    record StringLiteral(String lexeme) implements Token { }
+
+    // Keywords.
+    record And() implements Token { }
+    record Class() implements Token { }
+    record Else() implements Token { }
+    record False() implements Token { }
+    record Fun() implements Token { }
+    record For() implements Token { }
+    record If() implements Token { }
+    record Nil() implements Token { }
+    record Or() implements Token { }
+    record Print() implements Token { }
+    record Return() implements Token { }
+    record Super() implements Token { }
+    record This() implements Token { }
+    record True() implements Token { }
+    record Var() implements Token { }
+    record While() implements Token { }
+}
